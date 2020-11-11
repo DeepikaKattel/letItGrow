5c9b82236e16f4f54076a3d4daab10941806516a
Deepika <deepika.kattel80@gmail.com>
2020-11-11 16:34:34 +0545

    Added google map and form in contact blade

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -74,30 +74,7 @@
 			</div>
 
 			<!---Top Menu--->
-			<div id="cssmenu" >
-				<ul>
-				   <li class="active"><a href="index.html"><span>zMatcha</span></a></li>
-				   <li class="has-sub"><a href="#"><span>Category</span></a>
-					  <ul>
-						 <li class="has-sub"><a href="#"><span>Item 1</span></a>
-							<ul>
-							   <li><a href="#"><span>Sub Item</span></a></li>
-							   <li class="last"><a href="#"><span>Sub Item</span></a></li>
-							</ul>
-						 </li>
-						 <li class="has-sub"><a href="#"><span>Item 2</span></a>
-							<ul>
-							   <li><a href="#"><span>Sub Item</span></a></li>
-							   <li class="last"><a href="#"><span>Sub Item</span></a></li>
-							</ul>
-						 </li>
-					  </ul>
-				   </li>
-				   <li><a href="archive.html"><span>Archive</span></a></li>
-				   <li><a href="single.html"><span>About</span></a></li>
-				   <li class="last"><a href="contact.html"><span>Contact</span></a></li>
-				</ul>
-			</div>
+			@include('header')
 
 		</div>
 	</header>
@@ -117,7 +94,7 @@
 				<h1 class="">Contact Us</h1>
 				<div class="contact-map_wrapper wow fadeIn" data-wow-delay=".1s" data-wow-duration="1.5s">
 					<!-- Map -->
-					<div id="map" style="height: 550px;"></div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3533.096909160508!2d85.31605341458263!3d27.683399533151924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19f1a12ed15f%3A0xd2addf7cee6a8e0b!2sLet%20IT%20Grow!5e0!3m2!1sen!2snp!4v1605081029408!5m2!1sen!2snp" width="1170" height="500" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
 					<!-- Show Info Button -->
 					<div class="show-info-link">
@@ -131,26 +108,24 @@
 								  <!-- Phone -->
 								  <div class="sm-1-2 address-group">
 									<span>Phone</span>
-									<a href="#">+ 123 4567 890</a>
-									<a href="#">+ 123 7654 098</a>
+									<a href="#">+ 01-5901614</a>
 								  </div>
 								  <!-- Address -->
 								  <div class="sm-1-2 address-group">
 									<span>Address</span>
-									<p>1200 some street, il, US</p>
+									<p>Pulchowk, Lalitpur</p>
 								  </div>
 								</div>
 								<div class="row">
 								  <!-- Email -->
 								  <div class="sm-1-2 address-group">
 									<span>Email</span>
-									<a href="mailto:sayhello@email.com">sayhello@email.com</a>
+									<a href="mailto:sayhello@email.com">info@letitgrownepal.com </a>
 								  </div>
 								  <!-- Hours -->
 								  <div class="sm-1-2 address-group">
 									<span>Open Hours</span>
-									<p>Mon-Fri: 9am-5pm</p>
-									<p>Sat: 10am-1pm</p>
+									<p>Sun-Fri: 10am-5pm</p>
 								  </div>
 								</div>
 								<!-- Show Map Button -->
@@ -167,7 +142,7 @@
 							<div class="wrap-col">
 								<div class="wow fadeInLeft" data-wow-delay=".1s" data-wow-duration="1s">
 									<h3>Have any questions? Let's get in touch!</h3>
-									<p>Contact us if your are thinking of redesigning your garden, adding some garden features or simply looking for a bit of garden maintenance.</p>
+									<p>Contact us if your are interested.</p>
 								</div>
 							</div>
 						</div>
@@ -199,127 +174,7 @@
 	</section>
 
 	<!--////////////////////////////////////Footer-->
-	<footer id="page-footer">
-		<div class="zerogrid wrap-footer">
-			<div class="row">
-				<div class="sm-1-4 col-footer-1">
-					<div class="wrap-col">
-						<h3 class="widget-title">About Us</h3>
-						<p>Ut volutpat consectetur aliquam. Curabitur auctor in nis ulum ornare. Metus elit vehicula dui. Curabitur auctor in nis ulum ornare. Sed consequat, augue condimentum fermentum</p>
-						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque la udantium</p>
-					</div>
-				</div>
-				<div class="sm-1-4 col-footer-2">
-					<div class="wrap-col">
-						<h3 class="widget-title">Recent Post</h3>
-						<ul>
-							<li><a href="#">MOST VISITED COUNTRIES</a></li>
-							<li><a href="#">5 PLACES THAT MAKE A GREAT HOLIDAY</a></li>
-							<li><a href="#">PEBBLE TIME STEEL IS ON TRACK TO SHIP IN JULY</a></li>
-							<li><a href="#">STARTUP COMPANY???S CO-FOUNDER TALKS ON HIS NEW PRODUCT</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="sm-1-4 col-footer-3">
-					<div class="wrap-col">
-						<h3 class="widget-title">Tag Cloud</h3>
-						<a href="#">animals</a>
-						<a href="#">cooking</a>
-						<a href="#">countries</a>
-						<a href="#">city</a>
-						<a href="#">children</a>
-						<a href="#">home</a>
-						<a href="#">likes</a>
-						<a href="#">photo</a>
-						<a href="#">link</a>
-						<a href="#">law</a>
-						<a href="#">shopping</a>
-						<a href="#">skate</a>
-						<a href="#">scholl</a>
-						<a href="#">video</a>
-						<a href="#">travel</a>
-						<a href="#">images</a>
-						<a href="#">love</a>
-						<a href="#">lists</a>
-						<a href="#">makeup</a>
-						<a href="#">media</a>
-						<a href="#">password</a>
-						<a href="#">pagination</a>
-						<a href="#">wildlife</a>
-					</div>
-				</div>
-				<div class="sm-1-4 col-footer-4">
-					<div class="wrap-col">
-						<h3 class="widget-title">Gallery</h3>
-						<div class="row">
-							<div class="sm-1-3 xs-1-2">
-								<div class="wrap-col">
-									<a href="#"><img src="images/1.jpg"></a>
-								</div>
-							</div>
-							<div class="sm-1-3 xs-1-2">
-								<div class="wrap-col">
-									<a href="#"><img src="images/2.jpg"></a>
-								</div>
-							</div>
-							<div class="sm-1-3 xs-1-2">
-								<div class="wrap-col">
-									<a href="#"><img src="images/3.jpg"></a>
-								</div>
-							</div>
-							<div class="sm-1-3 xs-1-2">
-								<div class="wrap-col">
-									<a href="#"><img src="images/4.jpg"></a>
-								</div>
-							</div>
-							<div class="sm-1-3 xs-1-2">
-								<div class="wrap-col">
-									<a href="#"><img src="images/5.jpg"></a>
-								</div>
-							</div>
-							<div class="sm-1-3 xs-1-2">
-								<div class="wrap-col">
-									<a href="#"><img src="images/6.jpg"></a>
-								</div>
-							</div>
-							<div class="sm-1-3 xs-1-2">
-								<div class="wrap-col">
-									<a href="#"><img src="images/7.jpg"></a>
-								</div>
-							</div>
-							<div class="sm-1-3 xs-1-2">
-								<div class="wrap-col">
-									<a href="#"><img src="images/8.jpg"></a>
-								</div>
-							</div>
-							<div class="sm-1-3 xs-1-2">
-								<div class="wrap-col">
-									<a href="#"><img src="images/1.jpg"></a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="zerogrid bottom-footer">
-			<div class="row">
-				<div class="bottom-social">
-					<a href="#"><i class="fa fa-facebook"></i></a>
-					<a href="#"><i class="fa fa-instagram"></i></a>
-					<a href="#"><i class="fa fa-twitter"></i></a>
-					<a href="#"><i class="fa fa-google-plus"></i></a>
-					<a href="#"><i class="fa fa-pinterest"></i></a>
-					<a href="#"><i class="fa fa-vimeo"></i></a>
-					<a href="#"><i class="fa fa-linkedin"></i></a>
-					<a href="#"><i class="fa fa-youtube"></i></a>
-				</div>
-			</div>
-			<div class="copyright">
-				Copyright @ - <a href="https://www.zerotheme.com/free-website-templates" rel='nofollow'>Website Templates</a> by <a href="https://www.zerotheme.com">Zerotheme</a>
-			</div>
-		</div>
-	</footer>
+	 @include('footer')
 
 
 	<!-- ========== Scripts ========== -->
