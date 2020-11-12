189cff5843e1c2b84c92de89f59c51ecc8e1523e
Deepika <deepika.kattel80@gmail.com>
2020-11-12 12:11:51 +0545

    Added company details in registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,9 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'company_name' => ['required','string'],
+            'designation' => ['required','string'],
+            'company_type' => ['required','string']
         ]);
     }
 
@@ -68,6 +71,9 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'company_name' => $data['company_name'],
+            'designation' => $data['designation'],
+            'company_type' => $data['company_type'],
         ]);
     }
 }
